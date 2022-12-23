fe2ba1525d7eda63469f8d30006d0519118d8ef9
Abhoy <abhoygorai04@gmail.com>
2022-12-24 00:24:13 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+const { Socket } = require("dgram");
+
+const server = http.createServer(app);
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("Connection is made: ", socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log("User with ID: " + socket.id + " joined the room: " + data);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected " + socket.id);
+    })
+})
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log("server running in port " + PORT));
